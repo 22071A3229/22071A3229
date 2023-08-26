ca7e413e1afd25e4d205e9877abd37ad2b2c1e91
Mandadi Sai Varnika <131800083+22071A3229@users.noreply.github.com>
2023-08-27 00:31:36 +0530

    Update DFS.C

--- DFS.C
+++ DFS.C
@@ -1,52 +1,69 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 char stack[10];
-int top=-1;
+int top = -1;
+
 char pop();
 void push(char x);
-void main()
-{
-  int a[10][10],m,n;
-  int i,j,state[10];
-  char g[10],x;
-  printf("enter the no. of nodes of graph");
-  scanf("%d",&n);
-  printf("enter the nodes of the graph");
-  for(i=1;i<=n;i++)
-  { fflush(stdin);
-   g[i]=getchar();
-   }
-  printf("enter the adjacency matrix of the graph");
-  for(i=1;i<=n;i++)
-   for(j=1;j<=n;j++)
-     scanf("%d",&a[i][j]);
-    for(i=1;i<=n;i++)
-      state[i]=1;
- state[1]=2;
-push(g[1]);
-while(top!=-1)
-{
-  x=pop();
-  for(i=1;i<=n;i++)
-    if(x==g[i])
-     break;
-   state[i]=3;
-printf("%c",g[i]);
-for(j=1;j<=n;j++)
-{
- if((a[i][j]==1) && (state[j]==1))
-{
-   state[j]=2;
-   push(g[j]);
- }
+
+int main() {
+    char a[20][20], g[20], x;
+    int n, i, j, state[20];
+    
+    printf("Enter the number of nodes: ");
+    scanf("%d", &n);
+
+    printf("Enter %d nodes: ", n);
+    for (i = 1; i <= n; i++) {
+        fflush(stdin);
+        g[i] = getchar();
+    }
+
+    printf("Enter adjacency matrix:\n");
+    for (i = 1; i <= n; i++) {
+        for (j = 1; j <= n; j++) {
+            scanf(" %c", &a[i][j]); // Note the space before %c to consume the newline character from previous input
+        }
+    }
+
+    for (i = 1; i <= n; i++)
+        state[i] = 1;
+
+    printf("DFS Traversal: ");
+    state[1] = 2;
+    push(g[1]);
+
+    while (top >= 0) {
+        x = pop();
+
+        for (i = 1; i <= n; i++) {
+            if (x == g[i])
+                break;
+        }
+
+        if (state[i] != 3) {
+            state[i] = 3;
+            printf("%c ", g[i]);
+        }
+
+        for (j = n; j >= 1; j--) {
+            if ((a[i][j] == '1') && (state[j] == 1)) { // Note the comparison with character '1'
+                state[j] = 2;
+                push(g[j]);
+            }
+        }
+    }
+
+    printf("\n");
+    getch(); // Assuming this code is run on a Windows system and "getch()" is available
+    return 0;
 }
+
+void push(char x) {
+    stack[++top] = x;
 }
-}
-void push(char x)
-{
-top++;
-stack[top]=x;
-}
-char pop()
-{
- return(stack[top--]);
+
+char pop() {
+    return stack[top--];
 }
