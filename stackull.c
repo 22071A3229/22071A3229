e4c8dd4d85b0e57785b3b768be9f69b500467f9e
Mandadi Sai Varnika <131800083+22071A3229@users.noreply.github.com>
2023-08-27 00:32:56 +0530

    Update stackull.c

--- stackull.c
+++ stackull.c
@@ -1,95 +1,68 @@
 #include<stdio.h>
-#include<conio.h>
+#include<stdlib.h>
 struct node
 {
-int data;
-struct node*link;
-}*top=NULL,*nn,*temp;
-void push(int x)
+  int x;
+  struct node*link;
+}*temp,*nn,*top=NULL;
+int count()
 {
-nn=(struct node *)malloc(sizeof(struct node));
-printf("enter value:");
-scanf("%d",&x);
-nn->data=x;
-if(top==NULL)
-{
-top=nn;
-top->link=NULL;
+  int cnt=0;
+  for(temp=top;temp!=NULL;temp=temp->link)
+    cnt++;
+  return cnt;  
 }
-else
+void display()
 {
-nn->link=top;
-top=nn;
+  for(temp=top;temp!=NULL;temp=temp->link)
+    printf("%d ",temp->x);  
 }
-}
-int pop(void)
+void pop()
 {
-int ele;
-if(top==NULL)
+  if(f==NULL)
+    printf("Stack is empty\n");
+  else 
+    {
+      temp=top;
+      top=top->link;
+      temp->link=NULL;
+      printf("Deleted element is %d\n",temp->x);
+      free(temp);
+    }  
+}  
+void push(int k)
 {
-return(-1);
-}
-else
-{
-ele=top->data;
-temp=top;
-top=top->link;
-temp->link=NULL;
-free(temp);
-return(ele);
-}
-}
-void display(void)
-{
-int i;
-if(top==NULL)
-printf("stack empty");
-else
-{
-temp=top;
-while(temp!=NULL)
-{
-printf(" %d",temp->data);
-temp=temp->link;
-}
-}
-}
-int isempty()
-{
-if(top==NULL)
-return(0);
-else
-return(1);
+  nn=(struct node*)malloc(sizeof(struct node));
+  if(first==NULL)
+    {
+      top=nn;
+      top->link=NULL;
+    }
+  else
+    {  
+      nn->link=top;
+      top=nn;
+    }
+	nn->x=k;
 }
 int main()
 {
-int choice,ele,x;
-char ch;
-clrscr();
-do{
-printf("1:push 2:pop 3:display 4:isempty\n");
-scanf("%d",&choice);
-switch(choice)
-{
-case 1:push(x);
-       break;
-case 2:ele=pop();
-       printf("element popped is %d",ele);
-       break;
-case 3:display();
-       break;
-case 4:ele=isempty();
-       if(ele==0)
-       printf("stack empty");
-       else
-       printf("stack is not empty");
-       break;
-default:printf("wrong choice");
+  int k,choice;
+  printf("1.insert 2.delete 3.display 4.count  ");
+  do
+    {
+      scanf("%d",&choice);
+      switch(choice)
+        {
+          case 2: pop();break;
+          case 1: printf("Enter element to insert  ");
+                  scanf("%d",&k);
+                  push(k);break;
+          case 3: display();break;        
+          case 4: printf("No.of elements = %d\n",count());break;
+          default: printf("Wrong choice\n");        
+        }
+      printf("Continue 1/0  ");
+      scanf("%d",&k);  
+    }while(k==1);
 }
-printf("continue y/n");
-fflush(stdin);
-ch=getchar();
-}while(ch=='y');
-return(0);
-}
-
